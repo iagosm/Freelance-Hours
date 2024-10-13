eee5170d861566072568a5d3f2bd152c91692a2e
iagosm <iagosousa201486@gmail.com>
2024-10-13 19:12:28 -0300

    Adicionado o Timer do projeto

--- app/Livewire/Proposals/Create.php
+++ app/Livewire/Proposals/Create.php
@@ -12,12 +12,11 @@ class Create extends Component
 {
     
     public Project $project;
-    public bool $modal = true;
+    public bool $modal = false;
     #[Rule(['required', 'email'])]
     public string $email = '';
     #[Rule(['required', 'numeric', 'gt:0'])]
     public int $hours = 0;
-
     public bool $agree = false;
 
     public function render()
