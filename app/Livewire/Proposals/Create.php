15a524507a5739e16cd0a4c9e5d3cd12c8743692
iagosm <iagosousa201486@gmail.com>
2024-10-13 23:54:00 -0300

    Adicionando status de up and down nas propostas enviadas

--- app/Livewire/Proposals/Create.php
+++ app/Livewire/Proposals/Create.php
@@ -2,7 +2,10 @@
 
 namespace App\Livewire\Proposals;
 
+use App\Actions\ArrangePositions;
 use App\Models\Project;
+use App\Models\Proposal;
+use Illuminate\Support\Facades\DB;
 use Livewire\Attributes\Rule;
 use Livewire\Component;
 
@@ -31,11 +34,33 @@ class Create extends Component
             $this->addError('agree', 'Você precisa concordar com os termos de uso');
             return;
         }
-         $this->project->proposals()->updateOrCreate(
-            ['email' => $this->email,],
-            ['hours' => $this->hours,]
-        );
+        DB::transaction(function() {
+            $proposal = $this->project->proposals()->updateOrCreate(
+                ['email' => $this->email,],
+                ['hours' => $this->hours,]
+            );
+            $this->arrangePositions($proposal);
+        });
         $this->dispatch('proposal::created');
         $this->modal = false;
+    }
+
+    public function arrangePositions(Proposal $proposal)
+    {
+        $query = DB::select('
+        select *, row_number() over (order by hours asc) as newPosition
+        from proposals
+        where project_id = :project
+        ', ['project' => $proposal->project_id]);
+        $position = collect($query)->where('id', '=', $proposal->id)->first();
+        $otherProposal = collect($query)->where('position', '=', $position->newPosition)->first();
+        if ($otherProposal) {
+            $proposal->update(['position_status' => 'up']);
+            $oProposal = Proposal::find($otherProposal->id);
+            
+            $oProposal->update(['position_status' => 'down']);
+            // $oProposal->notify(new PerdeuMane($this->project));
+        }
+        ArrangePositions::run($proposal->project_id);
     }
 }
